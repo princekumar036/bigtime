027127a4d64a29aba4a3b9185d672c226033afb6
Prince Kumar <princekumar036@gmail.com>
2024-05-28 12:22:45 +0200

    fix favicon and title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,9 @@
     export let data: any;
 </script>
 
+<svelte:head>
+    <title>Bigtime</title> 
+</svelte:head>
+
 <Search />
 <Clock {data} />
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,5 +3,9 @@
     import Search from '$lib/Search.svelte';
 </script>
 
+<svelte:head>
+    <title>Bigtime</title> 
+</svelte:head>
+
 <Search />
 <Clock data='' />
