4b641013f3a2729f57aa6b38fadeb18169ec7644
Prince Kumar <princekumar036@gmail.com>
2024-05-28 12:32:47 +0200

    moved home button next to search

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,4 @@
   import "../app.css";
 </script>
 
-<a href="/" class="fixed top-0 left-0 bg-slate-800 py-3 px-5">
-  <svg class="opacity-70" xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><path fill="currentColor" d="m12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81zM12 3L2 12h3v8h6v-6h2v6h6v-8h3z"/></svg>
-</a>
-
 <slot />
